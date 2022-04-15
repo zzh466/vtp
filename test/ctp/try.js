7b435d5d783be67715817f1ece54593b6d80fa84
HanYuanDao <hanzhe.jason@gmail.com>
2022-04-15 09:36:12 +0800

    切换账号登录

--- test/ctp/try.js
+++ test/ctp/try.js
@@ -13,16 +13,42 @@ var m_CurrencyId = "CNY";
 var m_AppId = "simnow_client_test";
 var m_AuthCode = "0000000000000000";
 
-// var ctp1_TradeAddress = "tcp://180.168.146.187:10201";
-// var m_BrokerId = "9999";
-// var m_UserId = "136380";
-// var m_InvestorId = "136380";
-// var m_PassWord = "jpf000jpf";
-// var m_TradingDay = "20210805";
-// var m_AccountId = "136380";
-// var m_CurrencyId = "CNY";
-// var m_AppId = "simnow_client_test";
-// var m_AuthCode = "0000000000000000";
+ctp_info_1 = {
+"TradeAddress": ctp1_TradeAddress,
+"BrokerId": m_BrokerId,
+"UserId": m_UserId,
+"InvestorId": m_InvestorId,
+"PassWord": m_PassWord,
+"TradingDay": m_TradingDay,
+"AccountId": m_AccountId,
+"CurrencyId": m_CurrencyId,
+"AppId": m_AppId,
+"AuthCode": m_AuthCode,
+}
+
+var ctp1_TradeAddress_2 = "tcp://180.168.146.187:10201";
+var m_BrokerId_2 = "9999";
+var m_UserId_2 = "136380";
+var m_InvestorId_2 = "136380";
+var m_PassWord_2 = "jpf000jpf";
+var m_TradingDay_2 = "20210805";
+var m_AccountId_2 = "136380";
+var m_CurrencyId_2 = "CNY";
+var m_AppId_2 = "simnow_client_test";
+var m_AuthCode_2 = "0000000000000000";
+
+ctp_info_2 = {
+"TradeAddress": ctp1_TradeAddress_2,
+"BrokerId": m_BrokerId_2,
+"UserId": m_UserId_2,
+"InvestorId": m_InvestorId_2,
+"PassWord": m_PassWord_2,
+"TradingDay": m_TradingDay_2,
+"AccountId": m_AccountId_2,
+"CurrencyId": m_CurrencyId_2,
+"AppId": m_AppId_2,
+"AuthCode": m_AuthCode_2,
+}
 
 // var ctp1_TradeAddress = "tcp://180.169.95.252:41205";
 // var m_BrokerId = "5040";
@@ -35,6 +61,8 @@ var m_AuthCode = "0000000000000000";
 // var m_AppId = "client_syc_v21.4.1";
 // var m_AuthCode = "3DJRGBBOBJMVQQTT";
 
+now_user_index = 0;
+ctp_info_arr = [ctp_info_1, ctp_info_2];
 
 console.log("try.js starting");
 
@@ -43,13 +71,15 @@ var trader = ctp.createTrader();
 
 trader.on("connect", function (result) {
   console.log("in js code: ----> on connected , result=", result);
-  trader.reqAuthenticate(m_BrokerId, m_AccountId, m_AuthCode, m_AppId, function (result) {
+  trader.reqAuthenticate(ctp_info_arr[now_user_index].BrokerId, ctp_info_arr[now_user_index].AccountId, 
+    ctp_info_arr[now_user_index].AuthCode, ctp_info_arr[now_user_index].AppId, function (result) {
     console.log("in js code: reqAuthenticate result=", result);
   });
 });
 trader.on("rspAuthenticate", function (result) {
   console.log("in js code: ----> on rspAuthenticate , result=", result);
-  trader.reqUserLogin(m_BrokerId, m_AccountId, m_PassWord, function (result) {
+  trader.reqUserLogin(ctp_info_arr[now_user_index].BrokerId, ctp_info_arr[now_user_index].AccountId, 
+    ctp_info_arr[now_user_index].PassWord, function (result) {
     console.log("in js code: reqUserlogin result=", result);
   });
 })
@@ -83,10 +113,9 @@ trader.on("rspUserLogin", function (requestId, isLast, field, info) {
   //   console.log('settlementinfo return val is ' + result);
   // });
 
-  trader.reqUserLogout(m_BrokerId, m_UserId, function (field) {
-      console.log('reqUserLogout is callback');
-      console.log(field);
-  })
+  trader.reqQryTradingAccount(ctp_info_arr[now_user_index].BrokerId, ctp_info_arr[now_user_index].AccountId, function(result){
+    console.log("in js code:", 'reqQryTradingAccount return val is '+result);
+  });
 
   // var instrumentID = "";
   //   trader.reqQryInstrument(instrumentID, function (field) {
@@ -101,7 +130,14 @@ trader.on('rspUserLogout', function (requestId, isLast, field, info) {
   console.log("rspUserLogout: field", JSON.stringify(field));
   console.log('rspUserLogout callback');
 
-  trader.disposed()
+  if (now_user_index) {
+    now_user_index = 0;
+  } else {
+    now_user_index = 1;
+  }
+  trader.connect(ctp_info_arr[now_user_index].TradeAddress, undefined, 2, 0, function (result) {
+    console.log("in js code:", 'connect2 return val is ' + result);
+  });
 })
 
 var t_bInsertOrder = false;
@@ -184,8 +220,21 @@ trader.on('rspError', function (requestId, isLast, field) {
 //   console.log("rqInstrument: info", JSON.stringify(info));
 // })
 
-trader.connect(ctp1_TradeAddress, undefined, 2, 0, function (result) {
-  console.log("in js code:", 'connect return val is ' + result);
+trader.on('rqTradingAccount', function (requestId, isLast, field, info) {
+  console.log("rqTradingAccount");
+  console.log("rqTradingAccount: requestId", JSON.stringify(requestId));
+  console.log("rqTradingAccount: isLast", JSON.stringify(isLast));
+  console.log("rqTradingAccount: field", JSON.stringify(field));
+  console.log("rqTradingAccount: info", JSON.stringify(info));
+
+  trader.reqUserLogout(ctp_info_arr[now_user_index].BrokerId, ctp_info_arr[now_user_index].UserId, function (field) {
+      console.log('reqUserLogout is callback');
+      console.log(field);
+  })
+})
+
+trader.connect(ctp_info_arr[now_user_index].TradeAddress, undefined, 2, 0, function (result) {
+  console.log("in js code:", 'connect1 return val is ' + result);
 });
 
 console.log("in js code:", 'continute');
