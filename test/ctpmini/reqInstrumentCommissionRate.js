a640b098305100a45c0942b75fe71d27a47cb35b
HanYuanDao <hanzhe.jason@gmail.com>
2025-05-09 09:47:23 +0800

    提交测试代码

--- test/ctpmini/reqInstrumentCommissionRate.js
+++ test/ctpmini/reqInstrumentCommissionRate.js
@@ -1,20 +1,6 @@
 var ctp = require('../../build/Debug/ctpmini.node');
-ctp.settings({ log: false });
+ctp.settings({ log: true });
 
-// simnow hanzhe
-// var ctp1_TradeAddress = "tcp://180.168.146.187:10201";
-// var m_BrokerId = "9999";
-// var m_UserId = "187849";
-// var m_InvestorId = "187849";
-// var m_PassWord = "hzlianhd520!";
-// var m_TradingDay = "20210805";
-// var m_AccountId = "187849";
-// var m_CurrencyId = "CNY";
-// var m_AppId = "simnow_client_test";
-// var m_AuthCode = "0000000000000000";
-
-
-// CTP MINI
 var ctp1_TradeAddress = "tcp://58.240.234.72:21803";
 var m_BrokerId = "9060";
 var m_UserId = "199803";
@@ -51,10 +37,10 @@ trader.on("rspUserLogin", function (requestId, isLast, field, info) {
     console.log("rspUserLogin: info", JSON.stringify(info));
     login = true;
 
-    // 为空时查询所有
-    var instrumentID = "j";
-    trader.reqQryInstrument(instrumentID, function (field) {
-        console.log('reqQryInstrument is callback');
+    // 空字符串 查询所有
+    var instrumentId = 'jm2201';
+    trader.reqQryInstrumentCommissionRate(m_BrokerId, m_InvestorId, instrumentId, function (field) {
+        console.log('reqQryInstrumentCommissionRate is callback');
         console.log(field);
     })
 });
@@ -63,12 +49,12 @@ trader.on('rspError', function (requestId, isLast, field) {
     console.log(JSON.stringify(field));
 });
 
-trader.on('rqInstrument', function (requestId, isLast, field, info) {
-    console.log('rqInstrument');
+trader.on('rqInstrumentCommissionRate', function (requestId, isLast, field, info) {
+    console.log('rqInstrumentCommissionRate');
     console.log(JSON.stringify(requestId));
     console.log(JSON.stringify(isLast));
-    console.log("rqInstrument: field", JSON.stringify(field));
-    console.log("rqInstrument: info", JSON.stringify(info));
+    console.log("rqInstrumentCommissionRate: field", JSON.stringify(field));
+    console.log("rqInstrumentCommissionRate: info", JSON.stringify(info));
 })
 
 trader.connect(ctp1_TradeAddress, undefined, 2, 0, function (result) {
